d382382e987885ca00d4616e1be468b7374d1242
Raysingler25 <202503097411@alunos.estacio.br>
2025-03-27 21:02:22 -0300

    Desafio: nível aventureiro

--- xadrez.c
+++ xadrez.c
@@ -10,7 +10,7 @@ int main(){
     }
 
     printf("\n");
-    printf("Movimento Bispo: \n");
+    printf("***Movimento Bispo*** \n");
     //mover o Bispo 5 casas na diagonal para cima a direita
     int bispo = 0;
     
@@ -21,7 +21,7 @@ int main(){
     }
 
     printf("\n");
-    printf("Movimento Rainha: \n");
+    printf("***Movimento Rainha*** \n");
     //mover a Rainha em 8 casas para a esquerda
     int rainha = 0;
 
@@ -30,6 +30,21 @@ int main(){
         printf("Esquerda \n"); // Imprime a direção do movimento
         rainha++;
     } while (rainha < 8);
+
+    printf("\n");
+    printf("\n");
+
+    printf("***Movimento Cavalo***\n");
+    for (int cavalo = 0; cavalo < 2; cavalo++)
+    {
+        printf("Baixo \n"); // Imprime a direção do movimento
+    } for (int cavalo = 0; cavalo < 1; cavalo++)
+    {
+        printf("Esquerda \n");
+    }
+    
+    
+    
     
     
     
